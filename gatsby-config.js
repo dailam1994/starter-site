85bca80e520db96bb06bf32c7389d1e9eaafad72
dailam1994 <dai_lam88@hotmail.com>
2022-01-18 19:21:22 +1000

    site

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Starter Site',
+    description: 'Website Development Portfolio',
+    copyright: 'This website is copyright 2022 Starter Site',
+    contact: 'me@startersite.gov.com'
+  }
 }
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Culpa eu aliquip sit ex ullamco laborum nisi consectetur id incididunt amet. Labore ad consectetur deserunt mollit mollit magna laboris qui irure fugiat. Elit minim commodo et non aute irure duis eu proident cillum.</p>
+                <p>Culpa eu aliquip sit ex ullamco laborum nisi consectetur id incididunt amet. Labore ad consectetur deserunt mollit mollit magna laboris qui irure fugiat. Elit minim commodo et non aute irure duis eu proident cillum.</p>
+                <p>Culpa eu aliquip sit ex ullamco laborum nisi consectetur id incididunt amet. Labore ad consectetur deserunt mollit mollit magna laboris qui irure fugiat. Elit minim commodo et non aute irure duis eu proident cillum.</p>
+            </div>
+        </Layout>
+    )
+}
